0044617433cc2f29de4194596c28dbd54c935287
danglinh152 <danglinh.k4@gmail.com>
2025-07-04 01:08:48 +0700

    implement nest/config in order to get env values

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config'; // simple way: auto loads .env
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 8080);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
+
+  const config = app.get(ConfigService);
+
+  await app.listen(config.get<string>('PORT') ?? 8080);
 }
+
 bootstrap();
